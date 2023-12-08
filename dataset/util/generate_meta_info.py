153954405cc1910672d3a2586b98532d6c62a871
Kim2091 <62084776+Kim2091@users.noreply.github.com>
2023-12-08 00:15:38 -0500

    Update generate_meta_info.py
    Feature parity with pairdata

--- dataset/util/generate_meta_info.py
+++ dataset/util/generate_meta_info.py
@@ -16,31 +16,31 @@ def generate_meta_info_div2k():
         for filename in filenames:
             img_path = os.path.normpath(os.path.join(dirpath, filename))
             if ' ' in img_path:
-                raise ValueError(f'File or path name contains spaces: {img_path}')
+                logging.error(f'File or path name contains spaces: {img_path}')
+                continue
             img_list.append(img_path)
 
     if not img_list:
-        raise ValueError('No images found')
+        logging.error('No images found')
+        return
 
     with open(meta_info_txt, 'w') as f:
         for idx, img_path in enumerate(img_list):
             try:
                 img = Image.open(img_path)  # lazy load
-                width, height = img.size
                 mode = img.mode
+                if mode == 'RGBA':
+                    logging.error(f'Unsupported mode. Images should not be RGBA. Detected image mode: {mode}')
+                    continue
+
                 if mode == 'RGB':
                     n_channel = 3
                 elif mode == 'L':
                     n_channel = 1
                 else:
-                    raise ValueError(f'Unsupported mode {mode}.')
-
-                info = f'{img_path} ({height},{width},{n_channel})'
-                print(info)
-                f.write(f'{info}\n')
-                logging.info(f'Successfully processed {img_path}')
+                    logging.error(f'Unsupported mode. Images should be RGB or L. Detected image mode: {mode}')
+                    continue
             except Exception as e:
                 logging.error(f'Error processing {img_path}: {str(e)}')
-
 if __name__ == '__main__':
     generate_meta_info_div2k()
